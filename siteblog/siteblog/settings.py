5d07b816917b6c30965e04ce7d9bcb791fa2a15d
vladismeno <vladismeno@gmail.com>
2024-03-28 01:55:16 -0700

    Lesson 55: Модели проекта

--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'siteblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dartblog',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
