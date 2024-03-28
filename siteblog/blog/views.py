6077dcbda7ea9573e234ee63221424d6add3b0f3
vladismeno <vladismeno@gmail.com>
2024-03-28 00:32:53 -0700

    Lesson 54: Перенос шаблона в проект

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Привет мир!</h1>')
+    return render(request,'blog/index.html')
