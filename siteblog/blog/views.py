5f3926ccfb82462649fbd6a30cb060c8cf6140fd
vladismeno <vladismeno@gmail.com>
2024-03-30 00:36:11 -0700

    Lesson 62: Статьи по категориям

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -10,8 +10,23 @@ class Home(ListView):
     paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data()
+        context = super().get_context_data(**kwargs)
         context['title'] = 'Classic Blog Design'
+        return context
+
+
+class PostsByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
         return context
 
 
