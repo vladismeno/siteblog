272a799f9f9113e12f7fa0f9948beba7bb6bfc08
vladismeno <vladismeno@gmail.com>
2024-03-29 20:54:38 -0700

    Lesson 58: Тег шаблона меню

--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -212,3 +212,10 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
+    }
+}
